f4cfea1df810ddd095efb682ffe95e1236199340
Phil Stephenson <phil.stephenson@gmail.com>
2017-07-09 23:30:14 -0700

    Lab4 files

---
+++ labs/lab4/main.cpp
@@ -0,0 +1,18 @@
+#include "clist.h"
+using namespace std;
+
+int main()
+{
+    node * head = NULL;
+    node * copy = NULL;
+    build(head);
+    display(head);
+
+    //PLEASE PUT YOUR CODE HERE to call the function assigned
+
+
+    display(head); //resulting list after your function call!
+    destroy(head);
+    
+    return 0;
+}
